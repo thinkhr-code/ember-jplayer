14d88cb61223fe2298ef3e8ad574e497de74c907
John Ratcliff <john.w.ratcliff@biola.edu>
2017-10-05 14:57:54 +0000

    Upgrade to ember-cli 2.15

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,9 @@
-import resolver                  from './helpers/resolver';
-import prepTests                 from 'mammoth-test-helpers/test-support/prep-tests';
-import { setResolver }           from 'ember-qunit';
+import resolver from './helpers/resolver';
+import prepTests from 'mammoth-test-helpers/test-support/prep-tests';
+import { setResolver } from 'ember-qunit';
+import { start } from 'ember-cli-qunit';
 
 setResolver(resolver);
 
 prepTests();
+start();
